75e2d1f6cff0ec5cc84bb5a38dac5f1a43c6c690
Bledor Pireci <=>
2025-11-14 16:40:33 +0100

    port changes

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
-    console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-    )
+    '0.0.0.0',
+    () => {
+        console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    }
 );
